dc6a681440c27678725ac48afb4ee8e9c55e6e3a
shane <shane@vm>
2024-07-02 00:54:43 -0700

    Extract bindings and nix-index from .zshrc; add .bashrc

--- modules/shell.nix
+++ modules/shell.nix
@@ -2,19 +2,82 @@
 let
   cfg = config.shanetrs.shell // { all_features = cfg.bash.features ++ cfg.zsh.features; };
   feature-aliases = {
-    cat = mkIf (builtins.elem "bat" cfg.zsh.features) "bat";
-    ccat = mkIf (builtins.elem "bat" cfg.zsh.features) "command cat";
-    ccd = mkIf (builtins.elem "zoxide" cfg.zsh.features) "builtin cd";
-    eza = mkIf (builtins.elem "eza" cfg.zsh.features) (mkForce "eza --header -o");
-    f = mkIf (builtins.elem "fuck" cfg.zsh.features) "fuck";
-    grep = mkIf (builtins.elem "ugrep" cfg.zsh.features) "ugrep";
-    less = "less -R --use-color";
-    ls = mkIf (builtins.elem "eza" cfg.zsh.features) "eza";
-    tree = mkIf (builtins.elem "eza" cfg.zsh.features) "eza -T";
+    cat = mkIf (builtins.elem "bat" cfg.all_features) "bat";
+    ccat = mkIf (builtins.elem "bat" cfg.all_features) "command cat";
+    ccd = mkIf (builtins.elem "zoxide" cfg.all_features) "builtin cd";
+    eza = mkIf (builtins.elem "eza" cfg.all_features) (mkForce "eza --header -o");
+    f = mkIf (builtins.elem "fuck" cfg.all_features) "fuck";
+    grep = mkIf (builtins.elem "ugrep" cfg.all_features) "ugrep";
+    ls = mkIf (builtins.elem "eza" cfg.all_features) "eza";
+    tree = mkIf (builtins.elem "eza" cfg.all_features) "eza -T";
   };
-  inherit (lib) mkEnableOption mkForce mkIf mkMerge mkOption types;
+  inherit (lib) concatStringsSep mkEnableOption mkForce mkIf mkMerge mkOption types;
+  strIf = pred: str: (if pred then str else "");
 in {
   options.shanetrs.shell = let
+    defaults = rec {
+      "bash" = {
+        aliases = {
+          less = "less -R --use-color";
+          history-search = if (builtins.elem "fzf" cfg.bash.features) then
+            ''eval "$(fzf --tac < "$HISTFILE")"''
+          else
+            ''tac "$HISTFILE" | less'';
+        };
+        binds = {
+          "\\es" = ''
+            history-search
+          ''; # Alt + S
+        };
+        extraRc = ''
+          nix-run() {
+            NIXPKGS_ALLOW_UNFREE=1 nix shell --impure "pkgs#$1" \
+              --command sh -c "which ''${1#*.} &>/dev/null && exec ''${1#*.} ''${*:2}; exec ''${*:2}"
+          }
+          nix-shell() {(
+            for i in "$@"; do
+              if [ -n "$OPTION" ] || [[ "''${i:0:1}" == "-" ]]; then
+                ARGS+=" \"$i\""
+                OPTION=1; continue
+              fi
+              NIX_SHELL_PACKAGES+=" $i";
+              ARGS+=" \"pkgs#$i\""
+            done
+            eval "NIX_SHELL_PACKAGES=\"''${NIX_SHELL_PACKAGES#* }\" NIXPKGS_ALLOW_UNFREE=1 nix shell --impure $ARGS"
+          )}
+          where() { readlink -f "$(which "$@")"; }
+        '';
+      };
+      "zsh" = {
+        aliases.less = bash.aliases.less;
+        binds = {
+          "^[[H" = "beginning-of-line"; # Home
+          ";5H" = "beginning-of-line"; # Ctrl + Home
+          ";3H" = "beginning-of-line"; # Alt + Home
+
+          "^[[F" = "end-of-line"; # End
+          ";5F" = "end-of-line"; # Ctrl + End
+          ";3F" = "end-of-line"; # Alt + End
+
+          "^[[2~" = "overwrite-mode"; # Insert
+
+          "^[[3~" = "delete-char"; # Delete
+          "5~" = "kill-word"; # Ctrl + Delete
+          "3~" = "kill-word"; # Alt + Delete
+
+          ";5C" = "forward-word"; # Ctrl + Right
+          ";3C" = "forward-word"; # Alt + Right
+          ";5D" = "backward-word"; # Ctrl + Left
+          ";3D" = "backward-word"; # Alt + Left
+
+          "^[[5~" = "up-line-or-history"; # Page Up
+          "^[[6~" = "down-line-or-history"; # Page Down
+
+          "^[s" = "history-incremental-search-backward"; # Alt + S
+        };
+        extraRc = bash.extraRc;
+      };
+    };
     shell-body = shell: {
       enable = mkEnableOption "Customization and base CLI utilities for ${shell}";
       package = mkOption {
@@ -24,17 +87,23 @@ in {
       };
       aliases = mkOption {
         type = types.attrs;
-        default = { };
+        default = defaults.${shell}.aliases or { };
         example = { example-alias = "echo hello world!"; };
       };
-      features = mkOption {
-        type = types.listOf
-          (types.enum [ "bat" "eza" "fd" "fuck" "highlight" "fastfetch" "nix-index" "tldr" "ugrep" "zoxide" ]);
-        default = [ "bat" "eza" "fd" "fuck" "highlight" "fastfetch" "nix-index" "tldr" "ugrep" "zoxide" ];
+      binds = mkOption {
+        type = types.attrs;
+        default = defaults.${shell}.binds or { };
+        example = { "\\es" = "history-incremental-search-backward"; };
+      };
+      features = let
+        all-features = [ "bat" "eza" "fd" "fuck" "fastfetch" "fzf" "highlight" "nix-index" "tldr" "ugrep" "zoxide" ];
+      in mkOption {
+        type = types.listOf (types.enum all-features);
+        default = defaults.${shell}.features or all-features;
       };
       extraRc = mkOption {
-        type = types.str;
-        default = "";
+        type = types.lines;
+        default = defaults.${shell}.extraRc or "";
       };
     };
   in {
@@ -92,7 +161,7 @@ in {
             enable = true;
             hidden = true;
           };
-          fzf.enable = mkIf (builtins.elem "zoxide" cfg.all_features) true;
+          fzf.enable = mkIf (builtins.elem "fzf" cfg.all_features || builtins.elem "zoxide" cfg.all_features) true;
           tealdeer = mkIf (builtins.elem "tldr" cfg.all_features) {
             enable = true;
             settings.updates.auto_update = true;
@@ -125,17 +194,39 @@ in {
         enable = true;
         autosuggestions.enable = true;
         histSize = 10000; # home-manager default
-        promptInit = ''
-          ${builtins.readFile (functions.configs ".zshrc")}
-          ${if (builtins.elem "fuck" cfg.all_features) then ''
-            # cfg.all_features ? "fuck
-            eval $(thefuck --alias)
-            bindkey -s '\e\e' 'f\n'
-            bindkey -s '^[f' 'f\n'
-          '' else
-            ""}
-          ${cfg.zsh.extraRc}
-        '';
+        promptInit = concatStringsSep "\n"
+          (builtins.attrValues (builtins.mapAttrs (key: value: ''bindkey "${key}" "${value}"'') cfg.zsh.binds) ++ [
+            (builtins.readFile (functions.configs ".zshrc"))
+            (strIf (builtins.elem "fuck" cfg.all_features) ''
+              eval $(thefuck --alias)
+              bindkey -s '\e\e' 'f\n'
+              bindkey -s '^[f' 'f\n'
+            '')
+            (strIf (builtins.elem "nix-index" cfg.all_features) ''
+              SGR () { for i in "$@"; do echo -ne "\e[$i"m; done; }
+              nix-find() { nix-locate --no-group --top-level -r "$@"; }
+              command_not_found_handler() {(
+                CMD="$1"; IFS=$'\n'
+                if [ "$NIX_MISSING" = "never" ]; then
+                  echo "$(SGR 1 34)❭❭ $(SGR 0 1)$CMD$(SGR 0) not found! You can use $(SGR 1)nix-find -wtx /$CMD$(SGR 0) to find it" >&2
+                  exit 127
+                fi
+                PACKAGES=($(nix-locate --minimal --no-group --type x --type s --top-level --whole-name --at-root "/bin/$CMD"))
+                case "''${#PACKAGES}" in
+                  0) echo "$(SGR 1 34)❭❭ $(SGR 0 1)$CMD$(SGR 0) not found! Are you sure you've typed the command correctly?" >&2 ;;
+                  1) [ "$NIX_MISSING" = "auto" ] &&
+                      exec nix-shell "''${PACKAGES[1]}" --command "$@";
+                    echo -n "$(SGR 1 34)❭❭ $(SGR 0 1)$CMD$(SGR 0) not found! Would you like to bring $(SGR 1)''${PACKAGES[1]%.*}$(SGR 0) into scope? " >&2; read
+                    exec nix-shell "''${PACKAGES[1]}" --command "$@" ;;
+                  *) [ "$NIX_MISSING" = "always" ] &&
+                      exec nix-shell "''${PACKAGES[1]}" --command "$@";
+                    echo "$(SGR 1 34)❭❭ $(SGR 0 1)$CMD$(SGR 0) not found! Would you like to bring one of the following packages into scope?" >&2
+                    PS3=""; select PKG in ''${PACKAGES[@]%.*}; do exec nix-shell "$PKG" --command "$@"; done ;;
+                esac
+                exit 127
+              )}
+            '')
+          ]) + cfg.zsh.extraRc;
       };
       user = { config, ... }: {
         home.packages = with pkgs; [ zsh-completions ];
@@ -165,22 +256,22 @@ in {
       user.programs = {
         bash = {
           enable = true;
-          historyFile = ".config/.bash_history";
+          historyFile = "$HOME/.config/.bash_history";
+          historyControl = [ "erasedups" ];
         };
         fzf.enableBashIntegration = true;
         zoxide.enableBashIntegration = true;
       };
       programs.bash = {
         enableCompletion = true;
-        shellInit = ''
-          ${builtins.readFile (functions.configs ".bashrc")}
-          ${if (builtins.elem "fuck" cfg.all_features) then ''
-            # cfg.all_features ? "fuck
-            eval $(thefuck --alias)
-          '' else
-            ""}
-          ${cfg.bash.extraRc}
-        '';
+        promptInit = concatStringsSep "\n"
+          (builtins.attrValues (builtins.mapAttrs (key: value: "bind '\"${key}\":\"${value}\"'") cfg.bash.binds) ++ [
+            (builtins.readFile (functions.configs ".bashrc"))
+            (strIf (builtins.elem "fuck" cfg.all_features) "eval $(thefuck --alias)")
+            (strIf (builtins.elem "nix-index" cfg.all_features) ''
+              nix-find() { nix-locate --no-group --top-level -r "$@"; }
+            '')
+          ]) + cfg.bash.extraRc;
         shellAliases = feature-aliases // cfg.bash.aliases;
       };
     })
--- user/configs/global/all/.zshrc
+++ user/configs/global/all/.zshrc
@@ -1,6 +1,4 @@
-# Prompt Building
-
-SGR () { for i in "$@"; do echo -ne "\e[$i"m; done; }
+## Prompt Building
 
 # nix-shell packages
 [ -n "$NIX_SHELL_PACKAGES" ] && 
@@ -32,52 +30,7 @@ precmd() {
   RPS1="$(sed 's: *$::' <<< "$RPS1")"
 }
 
-# Custom Functions
-
-nix-run() {
-  NIXPKGS_ALLOW_UNFREE=1 nix shell --impure "pkgs#$1" \
-    --command sh -c "which ${1#*.} &>/dev/null && exec ${1#*.} ${*:2}; exec ${*:2}"
-}
-
-nix-shell() {(
-  for i in "$@"; do
-    if [ -n "$OPTION" ] || [[ "${i:0:1}" == "-" ]]; then
-      ARGS+=" \"$i\""
-      OPTION=1; continue
-    fi
-    NIX_SHELL_PACKAGES+=" $i";
-    ARGS+=" \"pkgs#$i\""
-  done
-  eval "NIX_SHELL_PACKAGES=\"${NIX_SHELL_PACKAGES#* }\" NIXPKGS_ALLOW_UNFREE=1 nix shell --impure $ARGS"
-)}
-
-where() { readlink -f "$(which "$@")"; }
-
-if which nix-index &>/dev/null; then
-  nix-find() { nix-locate --no-group --top-level -r "$@"; }
-  command_not_found_handler() {(
-    CMD="$1"; IFS=$'\n'
-    if [ "$NIX_MISSING" = "never" ]; then
-      echo "$(SGR 1 34)❭❭ $(SGR 0 1)$CMD$(SGR 0) not found! You can use $(SGR 1)nix-find -wtx /$CMD$(SGR 0) to find it" >&2
-      exit 127
-    fi
-    PACKAGES=($(nix-locate --minimal --no-group --type x --type s --top-level --whole-name --at-root "/bin/$CMD"))
-    case "${#PACKAGES}" in
-      0) echo "$(SGR 1 34)❭❭ $(SGR 0 1)$CMD$(SGR 0) not found! Are you sure you've typed the command correctly?" >&2 ;;
-      1) [ "$NIX_MISSING" = "auto" ] &&
-          exec nix-shell "${PACKAGES[1]}" --command "$@";
-        echo -n "$(SGR 1 34)❭❭ $(SGR 0 1)$CMD$(SGR 0) not found! Would you like to bring $(SGR 1)${PACKAGES[1]%.*}$(SGR 0) into scope? " >&2; read
-        exec nix-shell "${PACKAGES[1]}" --command "$@" ;;
-      *) [ "$NIX_MISSING" = "always" ] &&
-          exec nix-shell "${PACKAGES[1]}" --command "$@";
-        echo "$(SGR 1 34)❭❭ $(SGR 0 1)$CMD$(SGR 0) not found! Would you like to bring one of the following packages into scope?" >&2
-        PS3=""; select PKG in ${PACKAGES[@]%.*}; do exec nix-shell "$PKG" --command "$@"; done ;;
-    esac
-    exit 127
-  )}
-fi
-
-# Zsh Bindings & Preferences
+## Zsh Bindings & Preferences
 
 zle -N history-search
 zstyle ':completion:*' menu select
@@ -85,22 +38,3 @@ autoload -U select-word-style
 select-word-style bash
 
 typeset -g -A key
-
-key[Insert]='^[[2~'
-key[Home]='^[[H'
-key[PageUp]='^[[5~'
-key[Delete]='^[[3~'
-key[End]='^[[F'
-key[PageDown]='^[[6~'
-
-bindkey "$\{key[Insert]}" overwrite-mode
-bindkey "$\{key[Home]}" beginning-of-line ";5H" beginning-of-line ";3H" beginning-of-line
-bindkey "$\{key[PageUp]}" up-line-or-history
-bindkey "$\{key[Delete]}" delete-char
-bindkey "$\{key[End]}" end-of-line ";5F" end-of-line ";3F" end-of-line
-bindkey "$\{key[PageDown]}" down-line-or-history
-bindkey "5~" kill-word "3~" kill-word
-bindkey ";3C" .accept-line ";5C" .accept-line
-bindkey ";3C" forward-word ";5C" forward-word
-bindkey ";3D" backward-word ";5D" backward-word
-bindkey '^[s' history-incremental-search-backward
