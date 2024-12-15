f1a41018b9d480d883f684b1a75c4ed205a01d7f
shane <shane@lachesis>
2024-12-15 11:24:42 -0700

    Bandage fix for RTP microphone; minor package changes

--- packages/not-nice.nix
+++ packages/not-nice.nix
@@ -1,8 +1,12 @@
-{ writeShellScriptBin, ... }:
-writeShellScriptBin "not-nice" ''
-  export PATH="$PATH:/run/current-system/sw/bin/"
-  chrt -pf 99 $$
-  doas ionice -c1 -n0 -p$$
-  renice -n -20 -p $$
-  exec "$@"
-''
+{ pkgs, ... }:
+with pkgs;
+writeShellApplication {
+  name = "not-nice";
+  runtimeInputs = [ util-linux ];
+  text = ''
+    chrt -pf 99 $$
+    ''${SUDO:-doas} ionice -c1 -n0 -p$$
+    renice -n -20 -p $$
+    exec "$@"
+  '';
+}
